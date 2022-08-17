2a1ec95b0a67dc9bf71482833a923d3e3758d849
Nate Wessel <bike756@gmail.com>
2022-08-17 08:28:24 -0400

    add new func

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-export { wikidataQuery } from './query.js'
+export async function wikidataQuery(query){
+	const params = new URLSearchParams({format:'json',query})
+	return fetch(`https://query.wikidata.org/sparql?${params}`)
+		.then( response => response.json() )
+		.then( data => data.results.bindings )
+}
+
+
+export function values(IDs){
+	return IDs.map( id =>`wd:${id}`).join(' ')
+}
--- query.js
+++
@@ -1,8 +0,0 @@
-const API = 'https://query.wikidata.org/sparql'
-
-export async function wikidataQuery(query){
-	const params = new URLSearchParams({format:'json',query})
-	return fetch(`${API}?${params}`)
-		.then( response => response.json() )
-		.then( data => data.results.bindings )
-}
